05ac6fbeefb61d461c816def11f3da68f214508e
vuongxeu <zuong2k3@gmail.com>
2023-04-05 11:43:33 +0700

    all

---
+++ hienthira/hienthidaura.js
@@ -0,0 +1,13 @@
+function vuong1() {
+    alert("bạn muốn mua tivi")
+}
+function vuong2(){
+    document.getElementById('a1').innerHTML = "đến điện máy xanh"
+}
+function vuong3(){
+    document.write('bạn muốn mua tụ lặn')
+}
+function vuong4() {
+    console.log('đến điện máy xanh')
+
+}
---
+++ nhung/nhungs.js
@@ -0,0 +1,3 @@
+function baba(){
+    alert('haha');
+}
