bacd4362b81765a820dcd793de1212bcc39f2458
vuongxeu <zuong2k3@gmail.com>
2023-04-05 13:58:36 +0700

    khaibao

---
+++ khaibaosudungbien/khaibaosodugbien.js
@@ -0,0 +1,24 @@
+let a = 212222
+let b = 20000.233333
+let c = "phelebuche"
+let d = true
+document.write('a= ' +a)
+document.write('<br>')
+document.write('b= ' +b)
+    document.write('<br>')
+document.write('c= ' +c)
+        document.write('<br>')
+document.write('d= ' +d)
+            document.write('<br>')
+let width = 200
+let height = 100
+let cv = width*height
+document.write('dien tich = ' + cv)
+let x = prompt('nhập x')
+y = prompt('nhập y')
+if (x/y){
+    alert('chia hết')
+}
+
+
+
