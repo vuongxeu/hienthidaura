027667fca605832fb95b7ea1272c1d03d5ea7b1c
vuongxeu <zuong2k3@gmail.com>
2023-04-05 15:51:52 +0700

    nhuthat

---
+++ dtbdientichchuvi/dtbchuvidientich.js
@@ -0,0 +1,29 @@
+let ly =+prompt("nhập điểm lí")
+let hoa =+prompt( "nhập điểm hóa")
+let sinh  =+prompt("nhập điểm sinh")
+let tong =ly+hoa+sinh
+tb =tong/3
+document.write("tong= " + tong)
+document.write("<br>")
+document.write("dtb = " + tb)
+document.write("<br>")
+
+
+
+
+let c = +prompt("nhập độ C")
+f = c*9/5+32
+document.write("độ F = " + f)
+document.write("<br>")
+
+
+
+
+
+let a = +prompt("nhập bán kính")
+const pi = 3.141592
+cv = 2*a*pi
+dt = a*a*pi
+document.write("chu vi tron = " + cv)
+document.write("<br>")
+document.write("dien tich tron = " + dt)
